4aedcbacd9045661071e305f8349b6110f4b3491
Clemens Hug <clemens.hug@gmail.com>
2023-12-12 17:31:43 -0500

    Specify output dir in Salmon script

--- job-scripts/salmon.sh
+++ job-scripts/salmon.sh
@@ -7,33 +7,35 @@
 #SBATCH -o logs/%x_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e logs/%x_%j.err                 # File to which STDERR will be written, including job ID (%j)
 
+# comm -1 -3 <(ls -1 quants | grep -e .done | sed s/.done//g | sort | uniq) <(find raw/SRP* -name '*_1.fastq.gz' | sed s/_1.fastq.gz//g | sed 's/raw\/SRP.*\///g' | sort | uniq)
 # USAGE EXAMPLE
 # sbatch salmon.sh /n/scratch3/users/c/ch305/rna-editing/rna-seq/fastq/Homo_sapiens.GRCh38.gentrome_including_variants_index 962_TCX_2
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate salmon
 
-OUTPUT_PATH=quants
-
 set -eux
+INDEX="$1"
+OUTPUT_PATH="$2"
 
 # argument 1 contains path to salmon index
-# 2 onwards contains sample paths
+# argument 1 contains path to output folder
+# 3 onwards contains sample paths
 args=("$@")
-for i in "${args[@]:1}"
+for i in "${args[@]:2}"
 do
 dir=${i%/*}
 file="${i##*/}"
 echo "$SLURM_JOB_ID" > "${OUTPUT_PATH}/${file}.lock"
 # --recoverOrphans causes segfaults at the moment
 if [ -f "${i}_2.fastq.gz" ]; then
-  salmon quant -i "$1" -l A \
+  salmon quant -i "$INDEX" -l A \
     --seqBias --gcBias --posBias --softclip -p 4 \
     -1 "${i}_1.fastq.gz" \
     -2 "${i}_2.fastq.gz" \
     -o "${OUTPUT_PATH}/${file}"
 else
-  salmon quant -i "$1" -l A \
+  salmon quant -i "$INDEX" -l A \
     --seqBias --gcBias --posBias --softclip -p 4 \
     -r "${i}_1.fastq.gz" \
     -o "${OUTPUT_PATH}/${file}"
