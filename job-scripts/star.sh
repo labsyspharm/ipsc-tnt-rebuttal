4cd7dbac0894c1184cac9243fcb56fb4bb4e1716
Clemens Hug <clemens.hug@gmail.com>
2024-02-19 17:16:05 -0500

    Output folder as argument

--- job-scripts/star.sh
+++ job-scripts/star.sh
@@ -8,7 +8,7 @@
 #SBATCH -e logs/%x_%j.err                 # File to which STDERR will be written, including job ID (%j)
 
 #
-# comm -1 -3 <(ls -1 star | grep -e .done -e .lock | sed s/.done//g | sed s/.lock//g | sort | uniq) <(ls -1 trimmed | grep _1.fastq.gz | sed s/_1.fastq.gz//g | sort | uniq) |
+# comm -1 -3 <(ls -1 star | grep -e .done -e .lock | sed s/.done//g | sed s/.lock//g | sort | uniq) <(find raw/SRP* -name '*_1.fastq.gz' | sed s/_1.fastq.gz//g | sed 's/raw\/SRP.*\///g' | sort | uniq) |
 #  parallel sbatch star.sh /n/scratch3/users/c/ch305/indices/hg38_107/star trimmed/{} star_first/*SJ.out.tab
 
 # USAGE EXAMPLE
@@ -16,14 +16,15 @@
 
 module load gcc/6.2.0 star/2.7.9a
 
-OUTPUT_PATH=star
+OUTPUT_PATH="$2"
 
 set -eux
 
 # argument 1 contains path to salmon index
-# 2 onwards contains sample paths
+# argument 2 contains path to output folder
+# 3 onwards contains sample paths
 args=("$@")
-for i in "${args[@]:1}"
+for i in "${args[@]:2}"
 do
 dir=${i%/*}
 file="${i##*/}"
