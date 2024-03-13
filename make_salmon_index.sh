5bc7112b1ae1d185e0b1a4dbd2b32f9fc8a866ef
Clemens Hug <clemens.hug@gmail.com>
2024-03-13 16:33:26 -0400

    Cleanup for sharing

--- make_salmon_index.sh
+++ make_salmon_index.sh
@@ -19,15 +19,3 @@ cat Homo_sapiens.GRCh38.cdna.all.fa.gz \
 salmon index -t Homo_sapiens.GRCh38.gentrome_including_sendai.fa.gz \
   -d decoys.txt -p 12 \
   -i Homo_sapiens.GRCh38.gentrome_including_sendai_index
-
-# Include variants of Yamanaka transcripts including and exluding UTR
-# Filter out the UTR containing variant from the Ensembl cDNA
-# <(gzip -cd Homo_sapiens.GRCh38.cdna.all.fa.gz | python ../filter_fasta.py - - 'ENST00000259915.13|ENST00000374672.5|ENST00000621592.8|ENST00000325404.3' | gzip -c) \
-cat <(gzip -c "../raw/sequences/yamanaka_utr_variants.fa") \
-  <(gzip -cd Homo_sapiens.GRCh38.cdna.all.fa.gz | python ../filter_fasta.py - - 'ENSG00000181449.4|ENSG00000204531.21|ENSG00000136997.22|ENSG00000136826.15' | gzip -c) \
-  Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz > \
-  Homo_sapiens.GRCh38.yamanaka_utr_variants.fa.gz
-
-salmon index -t Homo_sapiens.GRCh38.yamanaka_utr_variants.fa.gz \
-  -d decoys.txt -p 12 \
-  -i Homo_sapiens.GRCh38.yamanaka_utr_variants_index
